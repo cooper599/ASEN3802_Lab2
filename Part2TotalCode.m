fcaebb4cd13afb7d54265313248ccf683ad6007a
cooper599 <cowa7997@colorado.edu>
2026-03-10 11:41:41 -0600

    Add files via upload

--- Part2TotalCode.m
+++ Part2TotalCode.m
@@ -170,6 +170,10 @@ for i = 1:N
     ualt1000(i) = intercepts(1) + H(1)*xp2 + fssum1000;
 end
 
+% Fourier Number Calculation
+Fo1 = alpha*t1/L^2;
+Fo1000 = alpha*t2/L^2;
+
 % Creating figures to show convergence after n terms
 figure();
 subplot(2,1,1)
@@ -226,22 +230,61 @@ for i = 1:length(a)
     legend("TC1_{ana}", "TC1_{exp}", "TC2_{ana}", "TC2_{exp}", "TC3_{ana}", "TC3_{exp}", "TC4_{ana}", "TC4_{exp}",...
         "TC5_{ana}", "TC5_{exp}", "TC6_{ana}", "TC6_{exp}", "TC7_{ana}", "TC7_{exp}", "TC8_{ana}", "TC8_{exp}", location="eastoutside")
     
-    % Model II Plot/Task 4
-    Mexp = slopes_0(i);
+    % Model II plot
     T0 = intercepts_0(i);
+    M = slopes_0(i);
     figure(); hold on; grid on;
     for j = 1:8
-        % For scale factor did H-M instead M-H so (-1)^n could be used 
-        % absorbed negative sign into expression
-        Tana = rodTransientModel(thermocoupleLoc(j), t, L, alpha, H_IA, T0, N, (H_IA-Mexp));
+        Tana = rodTransientModel(thermocoupleLoc(j), t, L, alpha, H_IB, T0, N, (H_IB-M));
         plot(t, Tana, 'LineWidth', 1.2);
         plot(t, Texpt(:,j), '--', 'LineWidth', 1.0);
     end
     title("Model II: ", fullname(i),Interpreter="none");
     xlabel("Time (s)"); ylabel("Temperature (\circ C)");
     legend("TC1_{ana}", "TC1_{exp}", "TC2_{ana}", "TC2_{exp}", "TC3_{ana}", "TC3_{exp}", "TC4_{ana}", "TC4_{exp}",...
-        "TC5_{ana}", "TC5_{exp}", "TC6_{ana}", "TC6_{exp}", "TC7_{ana}", "TC7_{exp}", "TC8_{ana}", "TC8_{exp}", location="eastoutside")
+        "TC5_{ana}", "TC5_{exp}", "TC6_{ana}", "TC6_{exp}", "TC7_{ana}", "TC7_{exp}", "TC8_{ana}", "TC8_{exp}", location="eastoutside");
 end
+%% Task 4
+% Getting transient responses for each thermocouple
+% for i = 1:length(a)
+%     thermotransient.(fullname(i)) = data.(fullname(i))(:,2:end);
+%     time.(fullname(i)) = data.(fullname(i))(:,1);
+% end
+% 
+% for i = 1:length(a)
+%     % Getting M and H values
+%     T0 = intercepts_0(i);
+%     M = slopes_0(i);
+%     Hloop = H(i);
+% 
+%     % Calculating alpha
+%     mat = names(i);
+%     alpha = k.(mat)/(rho.(mat)*cp.(mat));
+% 
+%     % Creating Figures
+%     figure(); hold on;
+%     % Transient response
+%     % Summation up to 10 terms
+%     for ii = 1:length(thermocoupleLoc)
+%         x = thermocoupleLoc(ii);
+%         summation = 0;
+%         for n = 1:10
+%             templambda = (2*n-1)*pi/(2*L);
+%             tempbn =  (8*L*(-1)^(n+1))/((2*n-1)^2*pi^2) * (M-Hloop);
+%             tempsum = tempbn*sin(templambda*x).*exp((-templambda^2*alpha).*time.(fullname(i)));
+%             summation = summation + tempsum;
+%         end
+%         transientresp = T0 + Hloop*thermocoupleLoc(ii) + summation;
+%         % Plotting analytical
+%         plot(time.(fullname(i)),transientresp,'-');
+%         %Plotting experimental
+%         plot(time.(fullname(i)),thermotransient.(fullname(i))(:,ii),'--');
+%     end
+%     % Label etc.
+%     xlabel("Time t (s)");
+%     ylabel("Temperature (\circ C)");
+%     title("Model IA: ", fullname(i),Interpreter="none");
+% end
 
 %% Functions:
 function T = rodTransientModel(x, t, L, alpha, Huse, T0use, N, bnScale)
